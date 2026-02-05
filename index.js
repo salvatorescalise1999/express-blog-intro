cb485461e46f72a7c8c39b9bd1ba27d23c5a0280
Salvatore Scalise <salvatorescalise1999@gmail.com>
2026-02-05 15:51:35 +0100

    feat: attivo la cartella public per uso di file statici

--- index.js
+++ index.js
@@ -2,10 +2,15 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+// attivazione della cartella public per uso file statici
+app.use(express.static('public'));
+
+//chiamata
+app.get("/", (req, res) => {
+    res.send("server del mio blog")
 })
 
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
